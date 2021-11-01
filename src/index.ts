9a08bbd35be548449f7bcd7ccf67799a73455d3e
Basabe-Gabriel <92063962+Basabe-Gabriel@users.noreply.github.com>
2021-11-01 20:22:46 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+let numero: number[] = new Array ();
+
+numero [0] = 10;
+numero [1] = 5;
+numero [2] = 99;
+numero [3] = 52;
+numero [4] = -52;
+numero [5] = 1;
+numero [6] = 0;
+numero [7] = 77;
+
+
+for let i = 0; i <= 7; i++{
+  console.log(numero[i]);
+}
